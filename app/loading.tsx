2412d8b09a0621ca6b7dc0fa2f8eda655c3b250f
fgrreloaded <nk.nitish.nk500@gmail.com>
2024-08-07 21:47:41 +0530

    chore: Update layout styles for authentication and loading pages

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,9 +2,9 @@
 
 export default function LoadingPage() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen w-full">
       <div className="animate-spin rounded-full h-12 w-12
-       border-t-2 border-b-2 border-[#ff8200]"></div>
+       border-t-2 border-b-2 border-[#8d867f]"></div>
     </div>
   );
 }
